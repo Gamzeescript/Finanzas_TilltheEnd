37b622f3ee1d4845cb4d46e13003129088d51b8e
PedruxMendoza <pedrux_mendoza@hotmail.com>
2022-11-16 21:46:13 -0600

    funcionia?

---
+++ view/layout/header.php
@@ -0,0 +1,12 @@
+<head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title><?php { echo $titulo; } ?></title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+        <link href="../props/css/styles.css" rel="stylesheet" />
+        <link rel="stylesheet" href="../props/css/card.css">
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+</head>
---
+++ view/layout/scripts.php
@@ -0,0 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="../props/js/scripts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="../props/assets/demo/chart-pie-demo.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+<script src="../props/js/datatables-simple-demo.js"></script>
